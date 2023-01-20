be3635de83c56ae0d84475a93f639150130ae009
josh <j.surujpaul22@gmail.com>
2023-01-19 20:50:11 -0500

    Added PostGres code to certain methods

--- src/main/java/com/joshua/BankApplication/dao/ClientDataAccess.java
+++ src/main/java/com/joshua/BankApplication/dao/ClientDataAccess.java
@@ -1,13 +1,23 @@
 package com.joshua.BankApplication.dao;
 
 import com.joshua.BankApplication.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 @Repository("postgres")
 public class ClientDataAccess implements ClientDao{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ClientDataAccess(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertClient(UUID id, Client client) {
         return 0;
@@ -15,12 +25,23 @@ public class ClientDataAccess implements ClientDao{
 
     @Override
     public List<Client> selectAllClient() {
-        return List.of(new Client(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM client";
+        return jdbcTemplate.query(sql, (resultset, i)->{
+             UUID id = UUID.fromString( resultset.getString("id"));
+             String name = resultset.getString("name");
+             return new Client(id, name);
+         } );
     }
 
     @Override
     public Optional<Client> selectClientByID(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM client WHERE id = ?";
+        Client client = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultset, i)->{
+            UUID clientID = UUID.fromString( resultset.getString("id"));
+            String name = resultset.getString("name");
+            return new Client(clientID, name);
+        } );
+        return Optional.ofNullable(client);
     }
 
     @Override
